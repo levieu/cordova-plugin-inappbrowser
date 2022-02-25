7912711d3ef5b08ef4982a9d8a0bca864be6b22e
Leonetti Vito <v.leonetti@almaviva.it>
2022-02-25 18:30:08 +0100

    manage send cookies on loadstop

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1428,6 +1428,8 @@ public class InAppBrowser extends CordovaPlugin {
                 injectDeferredObject("window.webkit={messageHandlers:{cordova_iab:cordova_iab}}", null);
             }
 
+            String cookies = CookieManager.getInstance().getCookie(url);
+
             // CB-10395 InAppBrowser's WebView not storing cookies reliable to local device storage
             if (android.os.Build.VERSION.SDK_INT >= 21) {
                 CookieManager.getInstance().flush();
@@ -1443,6 +1445,7 @@ public class InAppBrowser extends CordovaPlugin {
                 JSONObject obj = new JSONObject();
                 obj.put("type", LOAD_STOP_EVENT);
                 obj.put("url", url);
+                obj.put("cookies", cookies);
 
                 sendUpdate(obj, true);
             } catch (JSONException ex) {
